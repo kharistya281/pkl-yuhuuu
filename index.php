0b9cb7f0ae8390d631e3cf1741627cd8f883c2af
Kharisma Agustya Zahra Salsabilla <142194971+kharistya281@users.noreply.github.com>
2024-07-23 08:53:59 +0700

    add progress

---
+++ index.php
@@ -0,0 +1,67 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>SIP</title>
+    <!-- Fonts -->
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Catamaran:wght@100..900&family=Roboto:ital,wght@0,100;0,300;0,400;0,500;0,700;0,900;1,100;1,300;1,400;1,500;1,700;1,900&display=swap" rel="stylesheet">
+
+    <!-- Link css -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.3/font/bootstrap-icons.min.css">
+    <link rel="stylesheet" href="asset/style.css">
+    
+    
+</head>
+
+<body>
+    <nav class="navbar navbar-expand-md fixed-top mynavbar">
+        <div class="container-fluid">
+            <a class="navbar-brand" href="index.php">
+                <img src="/pkl/asset/img/logos1.png" alt="">
+            </a>
+            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+            </button>
+            <div class="collapse navbar-collapse" id="navbarNavDropdown">
+            <ul class="navbar-nav">
+                <li class="nav-item">
+                <a class="nav-link active" aria-current="page" href="#">Tentang</a>
+                </li>
+                <li class="nav-item">
+                <a class="nav-link active" aria-current="page" href="#">Kontak</a>
+                </li>
+                <li class="nav-item dropdown">
+                <a class="nav-link" href="/pkl/admin.php" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                    <i class="bi bi-person-circle"></i>
+                </a>
+                </li>
+            </ul>
+            </div>
+        </div>
+    </nav>
+    <section id="landing" class="landing w-100 h-100 p-3 mx-auto text-center d-flex 
+    justify-content-center align-items-center">
+        <div class="content">
+            <h1>Sistem Informasi Kepuasan Kegiatan B2SA</h1>
+            <h1>Dinas Pertanian dan Ketahanan Pangan Provinsi Jawa Timur</h1>
+            <div class="line"></div>
+            <a href="#" class="btn btn-lg mt-4">Umum</a>
+        </div>
+    </section>
+    <!-- <div class="content">
+        <div class="sidebar">
+
+        </div>
+    </div>
+    <div class="main-content">
+        <h1>Sistem Informasi Kepuasan Kegiatan B2SA</h1>
+        <h2>Dinas Pertanian dan Ketahanan Pangan Provinsi Jawa Timur</h2>
+    </div> -->
+
+
+</body>
+</html>
