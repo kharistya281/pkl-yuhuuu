b28fc4f645ffad081bfde5499bbe9f393e376301
Kharisma Agustya Zahra Salsabilla <142194971+kharistya281@users.noreply.github.com>
2024-07-24 14:13:13 +0700

    update admin, staff, tambahstaff

--- admin.php
+++ admin.php
@@ -1,10 +1,14 @@
 <!DOCTYPE html>
 <html lang="en">
   <head>
-    <title>AdminX - The last Admin template you'll ever need</title>
+    <title>PKL</title>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-
+    <!-- Fonts -->
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Catamaran:wght@100..900&family=Roboto:ital,wght@0,100;0,300;0,400;0,500;0,700;0,900;1,100;1,300;1,400;1,500;1,700;1,900&display=swap" rel="stylesheet">
+    
     <link rel="stylesheet" type="text/css" href="./dist/css/adminx.css" media="screen" />
 
     <!--
@@ -12,22 +16,22 @@
       Feel free to delete these if you don't need them in your project
     -->
   </head>
+
   <body>
     <div class="adminx-container">
       <nav class="navbar navbar-expand justify-content-between fixed-top">
-        <a class="navbar-brand mb-0 h1 d-none d-md-block" href="index.html">
-          <img src="./demo/img/logo.png" class="navbar-brand-image d-inline-block align-top mr-2" alt="">
-          AdminX
+        <a class="navbar-brand mb-0 h1 d-none d-md-block" href="admin.php">
+          <img src="./asset/img/logos1.png" class="navbar-brand-image d-inline-block align-top mr-2" alt="">
         </a>
 
         <form class="form-inline form-quicksearch d-none d-md-block mx-auto">
           <div class="input-group">
             <div class="input-group-prepend">
-              <div class="input-group-icon">
+              <!-- <div class="input-group-icon">
                 <i data-feather="search"></i>
               </div>
             </div>
-            <input type="text" class="form-control" id="search" placeholder="Type to search...">
+            <input type="text" class="form-control" id="search" placeholder="Type to search..."> -->
           </div>
         </form>
 
@@ -36,69 +40,6 @@
             <i data-feather="menu"></i>
           </a>
         </div>
-
-        <ul class="navbar-nav d-flex justify-content-end mr-2">
-          <!-- Notificatoins -->
-          <li class="nav-item dropdown d-flex align-items-center mr-2">
-            <a class="nav-link nav-link-notifications" id="dropdownNotifications" data-toggle="dropdown" href="#">
-              <i class="oi oi-bell display-inline-block align-middle"></i>
-              <span class="nav-link-notification-number">3</span>
-            </a>
-            <div class="dropdown-menu dropdown-menu-right dropdown-menu-notifications" aria-labelledby="dropdownNotifications">
-              <div class="notifications-header d-flex justify-content-between align-items-center">
-                <span class="notifications-header-title">
-                  Notifications
-                </span>
-                <a href="#" class="d-flex"><small>Mark all as read</small></a>
-              </div>
-
-              <div class="list-group">
-                <a href="#" class="list-group-item list-group-item-action unread">
-                  <p class="mb-1">Invitation for <strong>Lunch</strong> on <strong>Jan 12th 2018</strong> by <strong>Laura</strong></p>
-
-                  <div class="mb-1">
-                    <button type="button" class="btn btn-primary btn-sm">Accept invite</button>
-                    <button type="button" class="btn btn-outline-danger btn-sm">Decline</button>
-                  </div>
-
-                  <small>1 hour ago</small>
-                </a>
-
-                <a href="#" class="list-group-item list-group-item-action">
-                  <p class="mb-1"><strong class="text-success">Goal completed</strong><br />1,000 new members today</p>
-                  <small>3 days ago</small>
-                </a>
-
-                <a href="#" class="list-group-item list-group-item-action">
-                  <p class="mb-1 text-danger"><strong>System error detected</strong></p>
-                  <small>3 days ago</small>
-                </a>
-
-                <a href="#" class="list-group-item list-group-item-action">
-                  <p class="mb-1">Your task <strong>Design Draft</strong> is due today.</p>
-                  <small>4 days ago</small>
-                </a>
-              </div>
-
-              <div class="notifications-footer text-center">
-                <a href="#"><small>View all notifications</small></a>
-              </div>
-            </div>
-          </li>
-          <!-- Notifications -->
-          <li class="nav-item dropdown">
-            <a class="nav-link avatar-with-name" id="navbarDropdownMenuLink" data-toggle="dropdown" href="#">
-              <img src="https://s3.amazonaws.com/uifaces/faces/twitter/jsa/128.jpg" class="d-inline-block align-top" alt="">
-            </a>
-            <div class="dropdown-menu dropdown-menu-right" aria-labelledby="navbarDropdownMenuLink">
-              <a class="dropdown-item" href="#">My Profile</a>
-              <a class="dropdown-item" href="#">My Tasks</a>
-              <a class="dropdown-item" href="#">Settings</a>
-              <div class="dropdown-divider"></div>
-              <a class="dropdown-item text-danger" href="#">Sign out</a>
-            </div>
-          </li>
-        </ul>
       </nav>
 
       <!-- expand-hover push -->
@@ -106,7 +47,7 @@
       <div class="adminx-sidebar expand-hover push">
         <ul class="sidebar-nav">
           <li class="sidebar-nav-item">
-            <a href="index.html" class="sidebar-nav-link active">
+            <a href="admin.php" class="sidebar-nav-link active">
               <span class="sidebar-nav-icon">
                 <i data-feather="home"></i>
               </span>
@@ -122,282 +63,56 @@
           <li class="sidebar-nav-item">
             <a href="#" class="sidebar-nav-link">
               <span class="sidebar-nav-icon">
-                <i data-feather="layout"></i>
+                <i data-feather="activity"></i>
               </span>
               <span class="sidebar-nav-name">
-                Layout Options
-              </span>
-              <span class="sidebar-nav-end">
-                <span class="badge badge-info">4</span>
+                Kegiatan
               </span>
             </a>
           </li>
 
           <li class="sidebar-nav-item">
-            <a class="sidebar-nav-link collapsed" data-toggle="collapse" href="#example" aria-expanded="false" aria-controls="example">
+            <a href="#" class="sidebar-nav-link">
               <span class="sidebar-nav-icon">
-                <i data-feather="pie-chart"></i>
+                <i data-feather="users"></i>
               </span>
               <span class="sidebar-nav-name">
-                Charts
-              </span>
-              <span class="sidebar-nav-end">
-                <i data-feather="chevron-right" class="nav-collapse-icon"></i>
+                Mitra / Instansi
               </span>
             </a>
-
-            <ul class="sidebar-sub-nav collapse" id="example">
-              <li class="sidebar-nav-item">
-                <a href="./layouts/charts_chartjs.html" class="sidebar-nav-link">
-                  <span class="sidebar-nav-abbr">
-                    Ch
-                  </span>
-                  <span class="sidebar-nav-name">
-                    ChartJS
-                  </span>
-                </a>
-              </li>
-
-              <li class="sidebar-nav-item">
-                <a href="./layouts/charts_morris.html" class="sidebar-nav-link">
-                  <span class="sidebar-nav-abbr">
-                    Mo
-                  </span>
-                  <span class="sidebar-nav-name">
-                    Morris
-                  </span>
-                </a>
-              </li>
-            </ul>
           </li>
 
           <li class="sidebar-nav-item">
-            <a class="sidebar-nav-link collapsed" data-toggle="collapse" href="#navTables" aria-expanded="false" aria-controls="navTables">
+            <a href="staff.php" class="sidebar-nav-link">
               <span class="sidebar-nav-icon">
-                <i data-feather="align-justify"></i>
+                <i data-feather="user"></i>
               </span>
               <span class="sidebar-nav-name">
-                Tables
-              </span>
-              <span class="sidebar-nav-end">
-                <i data-feather="chevron-right" class="nav-collapse-icon"></i>
+                Staff
               </span>
             </a>
-
-            <ul class="sidebar-sub-nav collapse" id="navTables">
-              <li class="sidebar-nav-item">
-                <a href="./layouts/tables.html" class="sidebar-nav-link">
-                  <span class="sidebar-nav-abbr">
-                    Re
-                  </span>
-                  <span class="sidebar-nav-name">
-                    Regular Tables
-                  </span>
-                </a>
-              </li>
-
-              <li class="sidebar-nav-item">
-                <a href="./layouts/tables_data.html" class="sidebar-nav-link">
-                  <span class="sidebar-nav-abbr">
-                    Da
-                  </span>
-                  <span class="sidebar-nav-name">
-                    Data Tables
-                  </span>
-                </a>
-              </li>
-            </ul>
           </li>
 
           <li class="sidebar-nav-item">
-            <a class="sidebar-nav-link collapsed" data-toggle="collapse" href="#navForms" aria-expanded="false" aria-controls="navForms">
+            <a href="staff.php" class="sidebar-nav-link">
               <span class="sidebar-nav-icon">
-                <i data-feather="edit"></i>
+                <i data-feather="folder"></i>
               </span>
               <span class="sidebar-nav-name">
-                Forms
-              </span>
-              <span class="sidebar-nav-end">
-                <i data-feather="chevron-right" class="nav-collapse-icon"></i>
+                Data Responden
               </span>
             </a>
-
-            <ul class="sidebar-sub-nav collapse" id="navForms">
-              <li class="sidebar-nav-item">
-                <a href="./layouts/form_elements.html" class="sidebar-nav-link">
-                  <span class="sidebar-nav-abbr">
-                    El
-                  </span>
-                  <span class="sidebar-nav-name">
-                    Elements
-                  </span>
-                </a>
-              </li>
-
-              <li class="sidebar-nav-item">
-                <a href="./layouts/form_advanced.html" class="sidebar-nav-link">
-                  <span class="sidebar-nav-abbr">
-                    Ad
-                  </span>
-                  <span class="sidebar-nav-name">
-                    Advanced Elements
-                  </span>
-                </a>
-              </li>
-            </ul>
           </li>
 
           <li class="sidebar-nav-item">
-            <a class="sidebar-nav-link collapsed" data-toggle="collapse" href="#navUI" aria-expanded="false" aria-controls="navUI">
+            <a href="#" class="sidebar-nav-link-out">
               <span class="sidebar-nav-icon">
-                <i data-feather="grid"></i>
+                <i data-feather="log-out"></i>
               </span>
               <span class="sidebar-nav-name">
-                UI Elements
-              </span>
-              <span class="sidebar-nav-end">
-                    <i data-feather="chevron-right" class="nav-collapse-icon"></i>
+                Keluar
               </span>
             </a>
-
-            <ul class="sidebar-sub-nav collapse" id="navUI">
-              <li class="sidebar-nav-item">
-                <a href="./layouts/icons.html" class="sidebar-nav-link">
-                  <span class="sidebar-nav-abbr">
-                    Ic
-                  </span>
-                  <span class="sidebar-nav-name">
-                    Icons
-                  </span>
-                </a>
-              </li>
-
-              <li class="sidebar-nav-item">
-                <a href="./layouts/buttons.html" class="sidebar-nav-link">
-                  <span class="sidebar-nav-abbr">
-                    Bu
-                  </span>
-                  <span class="sidebar-nav-name">
-                    Buttons
-                  </span>
-                </a>
-              </li>
-
-              <li class="sidebar-nav-item">
-                <a href="./layouts/notifications.html" class="sidebar-nav-link">
-                  <span class="sidebar-nav-abbr">
-                    No
-                  </span>
-                  <span class="sidebar-nav-name">
-                    Notifications
-                  </span>
-                </a>
-              </li>
-
-              <li class="sidebar-nav-item">
-                <a href="./layouts/progress.html" class="sidebar-nav-link">
-                  <span class="sidebar-nav-abbr">
-                    Pr
-                  </span>
-                  <span class="sidebar-nav-name">
-                    Progress Bars
-                  </span>
-                </a>
-              </li>
-
-              <li class="sidebar-nav-item">
-                <a href="./layouts/tabs.html" class="sidebar-nav-link">
-                  <span class="sidebar-nav-abbr">
-                    Ta
-                  </span>
-                  <span class="sidebar-nav-name">
-                    Tabs
-                  </span>
-                </a>
-              </li>
-            </ul>
-          </li>
-
-          <li class="sidebar-nav-item">
-            <a class="sidebar-nav-link collapsed" data-toggle="collapse" href="#navExtra" aria-expanded="false" aria-controls="navExtra">
-              <span class="sidebar-nav-icon">
-                <i data-feather="layers"></i>
-              </span>
-              <span class="sidebar-nav-name">
-                Other Layouts
-              </span>
-              <span class="sidebar-nav-end">
-                <i data-feather="chevron-right" class="nav-collapse-icon"></i>
-              </span>
-            </a>
-
-            <ul class="sidebar-sub-nav collapse" id="navExtra">
-              <li class="sidebar-nav-item">
-                <a href="./layouts/login.html" class="sidebar-nav-link">
-                  <span class="sidebar-nav-abbr">
-                    Lo
-                  </span>
-                  <span class="sidebar-nav-name">
-                    Login
-                  </span>
-                </a>
-              </li>
-
-              <li class="sidebar-nav-item">
-                <a href="./layouts/signup.html" class="sidebar-nav-link">
-                  <span class="sidebar-nav-abbr">
-                    Si
-                  </span>
-                  <span class="sidebar-nav-name">
-                    Sign Up
-                  </span>
-                </a>
-              </li>
-
-              <li class="sidebar-nav-item">
-                <a href="./layouts/404.html" class="sidebar-nav-link">
-                  <span class="sidebar-nav-abbr">
-                    Nf
-                  </span>
-                  <span class="sidebar-nav-name">
-                    404 Error
-                  </span>
-                </a>
-              </li>
-
-              <li class="sidebar-nav-item">
-                <a href="./layouts/500.html" class="sidebar-nav-link">
-                  <span class="sidebar-nav-abbr">
-                    Is
-                  </span>
-                  <span class="sidebar-nav-name">
-                    500 Error
-                  </span>
-                </a>
-              </li>
-
-              <li class="sidebar-nav-item">
-                <a href="./layouts/timeline.html" class="sidebar-nav-link">
-                  <span class="sidebar-nav-abbr">
-                    Ti
-                  </span>
-                  <span class="sidebar-nav-name">
-                    Timeline
-                  </span>
-                </a>
-              </li>
-
-              <li class="sidebar-nav-item">
-                <a href="./layouts/invoice.html" class="sidebar-nav-link">
-                  <span class="sidebar-nav-abbr">
-                    In
-                  </span>
-                  <span class="sidebar-nav-name">
-                    Invoice
-                  </span>
-                </a>
-              </li>
-            </ul>
           </li>
         </ul>
       </div><!-- Sidebar End -->
@@ -413,7 +128,7 @@
             <!-- BreadCrumb -->
             <nav aria-label="breadcrumb" role="navigation">
               <ol class="breadcrumb adminx-page-breadcrumb">
-                <li class="breadcrumb-item"><a href="#">Home</a></li>
+                <li class="breadcrumb-item"><a href="admin.php">Home</a></li>
                 <li class="breadcrumb-item active" aria-current="page">Dashboard</li>
               </ol>
             </nav>
---
+++ tambahstaff.php
@@ -0,0 +1,235 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <title>AdminX - Advanced Elements</title>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+    <!-- Fonts -->
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Catamaran:wght@100..900&family=Roboto:ital,wght@0,100;0,300;0,400;0,500;0,700;0,900;1,100;1,300;1,400;1,500;1,700;1,900&display=swap" rel="stylesheet">
+
+    <link rel="stylesheet" type="text/css" href="./dist/css/adminx.css" media="screen" />
+  </head>
+  <body>
+    <div class="adminx-container">
+      <!-- Header -->
+      <nav class="navbar navbar-expand justify-content-between fixed-top">
+        <a class="navbar-brand mb-0 h1 d-none d-md-block" href="admin.php">
+          <img src="./asset/img/logos1.png" class="navbar-brand-image d-inline-block align-top mr-2" alt="">
+        </a>
+        <form class="form-inline form-quicksearch d-none d-md-block mx-auto">
+          <div class="input-group">
+            <div class="input-group-prepend">
+              <!-- <div class="input-group-icon">
+                <i data-feather="search"></i>
+                              </div>
+              </div>
+              <input type="text" class="form-control" id="search" placeholder="Type to search..."> -->
+          </div>
+        </form>
+
+        <div class="d-flex flex-1 d-block d-md-none">
+          <a href="#" class="sidebar-toggle ml-3">
+            <i data-feather="menu"></i>
+          </a>
+        </div>
+      </nav>
+      <!-- // Header -->
+
+      <!-- sidebar -->
+      <!-- expand-hover push -->
+      <div class="adminx-sidebar expand-hover push">
+        <ul class="sidebar-nav">
+          <li class="sidebar-nav-item">
+            <a href="admin.php" class="sidebar-nav-link">
+              <span class="sidebar-nav-icon">
+                <i data-feather="home"></i>
+              </span>
+              <span class="sidebar-nav-name">
+                Dashboard
+              </span>
+              <span class="sidebar-nav-end">
+
+              </span>
+            </a>
+          </li>
+
+          <li class="sidebar-nav-item">
+            <a href="#" class="sidebar-nav-link">
+              <span class="sidebar-nav-icon">
+                <i data-feather="activity"></i>
+              </span>
+              <span class="sidebar-nav-name">
+                Kegiatan
+              </span>
+            </a>
+          </li>
+
+          <li class="sidebar-nav-item">
+            <a href="#" class="sidebar-nav-link">
+              <span class="sidebar-nav-icon">
+                <i data-feather="users"></i>
+              </span>
+              <span class="sidebar-nav-name">
+                Mitra / Instansi
+              </span>
+            </a>
+          </li>
+
+          <li class="sidebar-nav-item">
+            <a href="staff.php" class="sidebar-nav-link active">
+              <span class="sidebar-nav-icon">
+                <i data-feather="user"></i>
+              </span>
+              <span class="sidebar-nav-name">
+                Staff
+              </span>
+            </a>
+          </li>
+
+          <li class="sidebar-nav-item">
+            <a href="staff.php" class="sidebar-nav-link">
+              <span class="sidebar-nav-icon">
+                <i data-feather="folder"></i>
+              </span>
+              <span class="sidebar-nav-name">
+                Data Responden
+              </span>
+            </a>
+          </li>
+
+          <li class="sidebar-nav-item">
+            <a href="#" class="sidebar-nav-link-out">
+              <span class="sidebar-nav-icon">
+                <i data-feather="log-out"></i>
+              </span>
+              <span class="sidebar-nav-name">
+                Keluar
+              </span>
+            </a>
+          </li>
+        </ul>
+      </div>
+      <!-- end sidebar -->
+
+      <!-- Main Content -->
+      <div class="adminx-content">
+        <div class="adminx-main-content">
+          <div class="container-fluid">
+            <!-- BreadCrumb -->
+            <nav aria-label="breadcrumb" role="navigation">
+              <ol class="breadcrumb adminx-page-breadcrumb">
+                <li class="breadcrumb-item"><a href="admin.php">Home</a></li>
+                <li class="breadcrumb-item"><a href="staff.php">Staff</a></li>
+                <li class="breadcrumb-item active  aria-current="page">Tambah Staff</li>
+              </ol>
+            </nav>
+
+            <div class="pb-3">
+              <h1>Tambah Staff</h1>
+            </div>
+
+            <div class="row">
+              <div class="col">
+                <div class="card mb-grid">
+                  <div class="card-header">
+                    <div class="card-header-title">Form Tambah Staff</div>
+                  </div>
+                  <div class="card-body">
+                    <div class="form-group">
+                      <label class="form-label">NIP</label>
+                      <input class="form-control mb-2 input-credit-card" type="text" placeholder="Masukkan NIP">
+                    </div>
+
+                    <div class="form-group">
+                      <label class="form-label">Nama</label>
+                      <input class="form-control input-date mb-2" type="text" placeholder="Masukkan Nama">
+                    </div>
+
+                    <div class="form-group">
+                      <label class="form-label">No Telp</label>
+                      <input class="form-control input-numeral mb-2" type="text" placeholder="Masukkan No Telp">
+                    </div>
+
+                    <div class="form-group">
+                      <label class="form-label">Email</label>
+                      <input class="form-control input-prefix mb-2" type="text" placeholder="Masukkan Email">
+                    </div>
+                  </div>
+                  <!-- <div class="card-footer">
+                    Cleave.js is a small library for input formatting. Check out their <a href="http://nosir.github.io/cleave.js/" target="_blank">documentation</a>. <strong>Nice to know:</strong> There are also Angular and ReactJS components available.
+                  </div> -->
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+      <!-- // Main Content -->
+    </div>
+
+    <!-- If you prefer jQuery these are the required scripts -->
+    <script src="https://code.jquery.com/jquery-3.2.1.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.3/umd/popper.min.js"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta.2/js/bootstrap.min.js"></script>
+    <script src="../dist/js/vendor.js"></script>
+    <script src="../dist/js/adminx.js"></script>
+
+    <script src="https://unpkg.com/feather-icons"></script>
+    <script>
+        // Initialize feather icons
+        feather.replace();
+    </script>
+    <!-- If you prefer vanilla JS these are the only required scripts -->
+    <!-- script src="../dist/js/vendor.js"></script>
+    <script src="../dist/js/adminx.vanilla.js"></script-->
+
+    <script>
+      var choices = new Choices('.js-choice');
+
+      var choices2 = new Choices('.js-choice-remove', {
+        removeItemButton: true,
+      });
+
+      var cleave = new Cleave('.input-credit-card', {
+        creditCard: true,
+        onCreditCardTypeChanged: function (type) {
+          // update UI ...
+        }
+      });
+
+      var cleave2 = new Cleave('.input-date', {
+        date: true,
+        datePattern: ['Y', 'm', 'd']
+      });
+
+      var cleave3 = new Cleave('.input-numeral', {
+        numeral: true,
+        numeralThousandsGroupStyle: 'thousand'
+      });
+
+      var cleave = new Cleave('.input-prefix', {
+          prefix: 'INVOICE-',
+          uppercase: true
+      });
+
+      flatpickr(".date-default", {
+        allowInput: true
+      });
+      flatpickr(".date-time", {
+        allowInput: true,
+        enableTime: true,
+      });
+      flatpickr(".date-human", {
+        allowInput: true,
+        altInput: true,
+      });
+      flatpickr(".date-inline", {
+        allowInput: true,
+        inline: true,
+      });
+    </script>
+  </body>
+</html>
